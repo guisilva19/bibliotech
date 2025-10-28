832d50383bd43f3d5c0adee262a5426dbe845a7d
Guilherme Silva Fernandes <2003silvagui@gmail.com>
2025-10-28 19:30:00 -0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Bibliotech",
+  description: "Bibliotech é uma plataforma de gerenciamento de biblioteca",
+};
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
